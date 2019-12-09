b8b6bf89caa2b097a667a953a1a1206e3e7f59b0
Xiao Ma <xiaoma@Xiaos-MacBook-Pro.local>
2019-12-08 16:03:49 -0800

    updated the readme

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
   name = 'tokenizer_xm',         # How you named your package folder (MyLib)
   packages = ['tokenizer_xm'],   # Chose the same as "name"
   version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Tokenizing with options to exclude contractions, lemmatize and stem.',   # Give a short description about your library
+  description = 'Tokenizing with options to include contractions, lemmatize and stem.',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type='text/markdown', 
   author = 'Xiao Ma',                   # Type in your name
   author_email = 'Marshalma0923@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ALaughingHorse/tokenizer_xm',   # Provide either the link to your github or to your website
